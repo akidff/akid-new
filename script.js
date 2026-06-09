c74f729ae2c52e73b86054dd984d0940b5705702
akidff <109821123+akidff@users.noreply.github.com>
2026-06-09 22:50:41 +0600

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-
-window.onload=()=>gsap.to('#loader',{opacity:0,duration:1,onComplete:()=>loader.remove()});
-document.addEventListener('mousemove',e=>{
-let c=document.querySelector('.cursor');
-c.style.left=e.clientX+'px';c.style.top=e.clientY+'px';
-});
-gsap.utils.toArray('.reveal').forEach(el=>{
-gsap.from(el,{y:100,opacity:0,duration:1,scrollTrigger:{trigger:el,start:'top 80%'}})
-});
